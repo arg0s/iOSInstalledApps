61b9ad85b7d658050de722d855df55f043e97b34
Araivnd Krishnaswamy <arvi@alumni.iastate.edu>
2012-09-02 16:05:10 +0530

    Tip of utility tools to fetch installed app details on iOS using a combination of process names and URL schemes. BSD Licensed

---
+++ LLInstalledApps.h
@@ -0,0 +1,17 @@
+//
+//  LLInstalledApps.h
+//  iOSInstalledApps
+//
+//  Created by Aravind Krishnaswamy on 02/09/12.
+//  Copyright (c) 2012 Aravind Krishnaswamy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLInstalledApps : NSObject
+
++ (NSArray *) runningApps;
+- (BOOL) appsInstalledWithScheme:(NSString *)applicationScheme;
++ (NSDictionary *) appsInstalledWithSchemes:(NSArray *)applicationSchemes;
+
+@end
---
+++ LLInstalledApps.m
@@ -0,0 +1,93 @@
+//
+//  LLInstalledApps.m
+//  iOSInstalledApps
+//
+//  Created by Aravind Krishnaswamy on 02/09/12.
+//  Copyright (c) 2012 Aravind Krishnaswamy. All rights reserved.
+//
+
+#import "LLInstalledApps.h"
+#import <sys/sysctl.h>
+
+@implementation LLInstalledApps
+
+static LLInstalledApps *singleton= nil;
+
++ (LLInstalledApps *)sharedInstance
+{
+    if (singleton == nil) {
+        singleton = [[LLInstalledApps alloc] init];
+    }
+    return singleton;
+}
+
+- (NSArray *) _sysctl_ps {
+    
+    int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0};
+    size_t miblen = 4;
+    size_t size;
+    int st = sysctl(mib, miblen, NULL, &size, NULL, 0);
+    struct kinfo_proc * process = NULL;
+    struct kinfo_proc * newprocess = NULL;
+    
+    do {
+        size += size / 10;
+        newprocess = realloc(process, size);
+        if (!newprocess){
+            if (process){
+                free(process);
+            }
+            return nil;
+        }
+        process = newprocess;
+        st = sysctl(mib, miblen, process, &size, NULL, 0);
+    } while (st == -1 && errno == ENOMEM);
+    
+    if (st == 0){
+        if (size % sizeof(struct kinfo_proc) == 0){
+            int nprocess = size / sizeof(struct kinfo_proc);
+            if (nprocess){
+                NSMutableArray * array = [[NSMutableArray alloc] init];
+                for (int i = nprocess - 1; i >= 0; i--){
+                    NSString * processID = [[NSString alloc] initWithFormat:@"%d", process[i].kp_proc.p_pid];
+                    NSString * processName = [[NSString alloc] initWithFormat:@"%s", process[i].kp_proc.p_comm];
+                    NSDictionary * dict = [[NSDictionary alloc] initWithObjects:[NSArray arrayWithObjects:processID, processName, nil] forKeys:[NSArray arrayWithObjects:@"ProcessID", @"ProcessName", nil]];
+                    [array addObject:dict];
+                }
+                free(process);
+                return array; 
+            }
+        }
+    }
+    
+    return nil;
+}
+
++ (NSArray *) runningApps{
+    
+    NSArray* foo = [[LLInstalledApps sharedInstance] _sysctl_ps];
+    assert(foo);
+    NSMutableArray* processes = [NSMutableArray array];
+    for (NSDictionary* spam in foo) {
+        [processes addObject:[spam objectForKey:@"ProcessName"]];
+    }
+    return [NSArray arrayWithArray:processes];
+}
+
+- (BOOL) appsInstalledWithScheme:(NSString *)applicationScheme {
+    return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:applicationScheme]];
+}
+
+
++ (NSDictionary* ) appsInstalledWithSchemes:(NSArray *)applicationSchemes {
+    LLInstalledApps* instance = [LLInstalledApps sharedInstance];
+    NSMutableDictionary* dict = [@{} mutableCopy];
+    for (NSString* scheme in applicationSchemes) {
+        [dict setObject:[NSNumber numberWithBool:
+                         [instance appsInstalledWithScheme:scheme]]
+                 forKey:scheme];
+    }
+    return dict;
+}
+
+@end
